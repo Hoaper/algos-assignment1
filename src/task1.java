d1aeda3b9ad138c4e36e144abc88266770859c8e
Hoaper <beeboplay@gmail.com>
2023-03-30 09:07:02 +0600

    added more examples

--- src/task1.java
+++ src/task1.java
@@ -8,6 +8,8 @@ public class task1 {
     }
 
     public static void main(String[] args){
-        System.out.println(findMin(5, new int[]{-123,-2131,1223,-12313243, -32}));
+        System.out.println(findMin(5, new int[]{-123,-2131,1223,-12313243, -32})); //-12313243
+        System.out.println(findMin(6, new int[]{-21,-23,1223,-1, -32, 4332})); //-32
+        System.out.println(findMin(7, new int[]{12,-2, 1, 4, -32, -33, -34})); //-34
     }
 }
