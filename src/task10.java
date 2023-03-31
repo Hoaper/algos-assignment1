351fefd0bef139f9737b799ed60923015b25067a
Hoaper <beeboplay@gmail.com>
2023-03-31 08:39:15 +0600

    task10 solved at task10.java with recursion

--- src/task10.java
+++ src/task10.java
@@ -1,15 +1,10 @@
 public class task10 {
-    public static int euclidGCDAlgo(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
+    public static int euclidGCDAlgoReq(int a, int b) {
+        if (b == 0) return a;
+        return euclidGCDAlgoReq(b, a % b);
     }
-
     public static void main(String[] args) {
-        System.out.println(euclidGCDAlgo(32, 48));   // 16
-        System.out.println(euclidGCDAlgo(10, 7));    // 1
+        System.out.println(euclidGCDAlgoReq(32, 48));   // 16
+        System.out.println(euclidGCDAlgoReq(10, 7));    // 1
     }
 }
