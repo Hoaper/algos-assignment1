b383e8c36a4e203ffed200223debd73f3d7134aa
Maxim <53962920+Hoaper@users.noreply.github.com>
2023-04-05 09:32:56 +0600

    decumentation added

--- src/task10.java
+++ src/task10.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
 
 public class task10 {
-    public static int euclidGCDAlgoReq(int a, int b) {
+    /*
+        @ getEuclidGCDAlgoReq - definition of gcd function
+        @   a                 - first number from user
+        @   b                 - second number from user
+        @ return              - returning gcd of function
+    */
+    public static int getEuclidGCDAlgoReq(int a, int b) {
         if (b == 0) return a; //base case
-        return euclidGCDAlgoReq(b, a % b); //Euclid's algorithm
+        return getEuclidGCDAlgoReq(b, a % b); //Euclid's algorithm
     }
     public static void main() {
         // getting input
@@ -13,6 +19,6 @@ public class task10 {
         System.out.println("Please write a num for b:");
         int b = sc.nextInt();
 
-        System.out.println("GCD is: " + euclidGCDAlgoReq(a, b));
+        System.out.println("GCD is: " + getEuclidGCDAlgoReq(a, b));
     }
 }
