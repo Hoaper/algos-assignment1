1af00edf4ad72dc00ef607fb0d960a5840d7e40d
Maxim <53962920+Hoaper@users.noreply.github.com>
2023-04-05 09:37:52 +0600

    docs added

--- src/task3.java
+++ src/task3.java
@@ -1,7 +1,12 @@
 import java.util.Scanner;
 
 public class task3 {
-    public static String checkPrime(int n) {
+    /*
+        @getResultOfCheckingPrime        - definition of checking prime function
+        @  n                             - number that need to check for prime
+        @return                          - returning average of array
+    */
+    public static String getResultOfCheckingPrime(int n) {
         if (n > 1) {
             for (int i = 2; i < Math.sqrt(n) + 1; i++) { // nums after the sqrt(n) are dividable by past
                 if (n % i == 0) return "Composite"; // check for a divisor if 0, then composite number
@@ -16,6 +21,6 @@ public class task3 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please write a num:");
         int n = sc.nextInt();
-        System.out.println(n + " is " + checkPrime(n) + "!");
+        System.out.println(n + " is " + getResultOfCheckingPrime(n) + "!");
     }
 }
